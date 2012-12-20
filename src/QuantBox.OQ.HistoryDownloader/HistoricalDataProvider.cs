0c100696166236fafe03451a65f18e954edd0183
伍侃 <wu-kan@163.com>
2012-12-20 17:55:27 +0800

    新添历史数据插件接口

---
+++ src/QuantBox.OQ.HistoryDownloader/HistoricalDataProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SmartQuant.Providers;
+using SmartQuant.Providers.Design;
+using System.ComponentModel;
+using SmartQuant.Instruments;
+
+namespace QuantBox.OQ.CTP
+{
+    public partial class HistoryDownloader : IHistoricalDataProvider
+    {
+        [TypeConverter(typeof(BarSizesTypeConverter))]
+        [Category(CATEGORY_HISTORICAL)]
+        public int[] BarSizes
+        {
+            get { return new int[] { 1, 5, 15, 30, 60, 120, 300, 900, 0x708, 0xe10}; }
+        }
+
+        [Category(CATEGORY_HISTORICAL)]
+        public HistoricalDataRange DataRange
+        {
+            get { return HistoricalDataRange.DaysAgo; }
+        }
+
+        [Category(CATEGORY_HISTORICAL)]
+        public HistoricalDataType DataType
+        {
+            get { return (HistoricalDataType.Bar | HistoricalDataType.Trade | HistoricalDataType.Quote); }
+        }
+
+        [Category(CATEGORY_HISTORICAL)]
+        public int MaxConcurrentRequests
+        {
+            get { return -1; }
+        }
+
+        public event HistoricalDataEventHandler HistoricalDataRequestCancelled;
+
+        public event HistoricalDataEventHandler HistoricalDataRequestCompleted;
+
+        public event HistoricalDataErrorEventHandler HistoricalDataRequestError;
+
+        public event HistoricalBarEventHandler NewHistoricalBar;
+
+        public event HistoricalMarketDepthEventHandler NewHistoricalMarketDepth;
+
+        public event HistoricalQuoteEventHandler NewHistoricalQuote;
+
+        public event HistoricalTradeEventHandler NewHistoricalTrade;
+
+        #region IHistoricalDataProvider
+        public void SendHistoricalDataRequest(HistoricalDataRequest request)
+        {
+            Instrument inst = request.Instrument as Instrument;
+            string altSymbol = inst.GetSymbol(Name);
+            string altExchange = inst.GetSecurityExchange(Name);
+        }
+
+        public void CancelHistoricalDataRequest(string requestId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ src/QuantBox.OQ.HistoryDownloader/Provider.cs
@@ -0,0 +1,174 @@
+﻿using SmartQuant;
+using SmartQuant.Providers;
+using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+
+namespace QuantBox.OQ.CTP
+{
+    public partial class HistoryDownloader : IProvider
+    {
+        private const string CATEGORY_ACCOUNT = "Account";
+        //private const string CATEGORY_BARFACTORY = "Bar Factory";
+        //private const string CATEGORY_DEBUG = "Debug";
+        //private const string CATEGORY_EXECUTION = "Settings - Execution";
+        private const string CATEGORY_HISTORICAL = "Settings - Historical Data";
+        private const string CATEGORY_INFO = "Information";
+        //private const string CATEGORY_NETWORK = "Settings - Network";
+        private const string CATEGORY_STATUS = "Status";
+
+        private ProviderStatus status;
+        private bool isConnected;
+
+        public event EventHandler Connected;
+        public event EventHandler Disconnected;
+        public event ProviderErrorEventHandler Error;
+
+        private bool disposed;
+
+
+        public HistoryDownloader()
+        {
+            status = ProviderStatus.Unknown;
+            SmartQuant.Providers.ProviderManager.Add(this);
+        }
+
+        //Implement IDisposable.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Free other state (managed objects).
+                }
+                // Free your own state (unmanaged objects).
+                // Set large fields to null.
+                Shutdown();
+                disposed = true;
+            }
+            //base.Dispose(disposing);
+        }
+
+        // Use C# destructor syntax for finalization code.
+        ~HistoryDownloader()
+        {
+            // Simply call Dispose(false).
+            Dispose(false);
+        }
+
+        #region IProvider
+        [Category(CATEGORY_INFO)]
+        public byte Id
+        {
+            get { return 57; }//不能与已经安装的插件ID重复
+        }
+
+        [Category(CATEGORY_INFO)]
+        public string Name
+        {
+            get { return "QBHD"; }//不能与已经安装的插件Name重复
+        }
+
+        [Category(CATEGORY_INFO)]
+        public string Title
+        {
+            get { return "QuantBox HistoryDownloader"; }
+        }
+
+        [Category(CATEGORY_INFO)]
+        public string URL
+        {
+            get { return "www.quantbox.cn"; }
+        }
+
+        [Category(CATEGORY_INFO)]
+        [Description("插件版本信息")]
+        public string Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version.ToString(); }
+        }
+
+        [Category(CATEGORY_STATUS)]
+        public bool IsConnected
+        {
+            get { return isConnected; }
+        }
+
+        [Category(CATEGORY_STATUS)]
+        public ProviderStatus Status
+        {
+            get { return status; }
+        }
+
+        public void Connect(int timeout)
+        {
+            Connect();
+            ProviderManager.WaitConnected(this, timeout);
+        }
+
+        public void Connect()
+        {
+            //_Connect();
+            isConnected = true;
+        }
+
+        public void Disconnect()
+        {
+            //_Disconnect();
+            isConnected = false;
+        }
+
+        public void Shutdown()
+        {
+            Disconnect();
+        }
+        
+        public event EventHandler StatusChanged;
+
+        private void ChangeStatus(ProviderStatus status)
+        {
+            this.status = status;
+            EmitStatusChangedEvent();
+        }
+
+        private void EmitStatusChangedEvent()
+        {
+            if (StatusChanged != null)
+            {
+                StatusChanged(this, EventArgs.Empty);
+            }
+        }
+
+        private void EmitConnectedEvent()
+        {
+            if (Connected != null)
+            {
+                Connected(this, EventArgs.Empty);
+            }
+        }
+
+        private void EmitDisconnectedEvent()
+        {
+            if (Disconnected != null)
+            {
+                Disconnected(this, EventArgs.Empty);
+            }
+        }
+
+        private void EmitError(int id, int code, string message)
+        {
+            if (Error != null)
+                Error(new ProviderErrorEventArgs(new ProviderError(Clock.Now, this, id, code, message)));
+        }
+        #endregion
+    }
+}
